4f4a90cec7f6ec3a4c1777a6ec705f233585daa5
Jared Ramirez <jaredramirez@me.com>
2015-04-14 17:12:55 -0500

    initial commit.

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jaredramirez on 4/14/15.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,22 @@
+package hello;
+
+/**
+ * Created by jaredramirez on 4/14/15.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getID(){
+        return id;
+    }
+
+    public String getContent(){
+        return content;
+    }
+}
